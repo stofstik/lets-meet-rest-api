1367180239e967cb49162614d36c233cf578bcb9
stofstik <verstegen.daan@gmail.com>
2015-12-17 20:29:39 +0100

    beautify

--- schemas/lobby.js
+++ schemas/lobby.js
@@ -2,12 +2,16 @@
 var mongoose = require('mongoose');
 
 var lobbySchema = mongoose.Schema({
-	name 	: String,
-	creator : { type : mongoose.Schema.Types.ObjectId, ref : 'User' },
-	users	: [ {
-		type	: mongoose.Schema.Types.ObjectId,
-		ref		: 'User' } ],
-	timeout : Date
+    name: String,
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    timeout: Date
 });
 
 // TODO some kind of method to remove this lobby after a certain amount of ms
